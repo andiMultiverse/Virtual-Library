335bbca08f382d64b41b750e2f0e2b326fc688b9
Andrea M <andrea.ramirez@andrea.ramirez-MacBook-Pro>
2024-10-24 14:40:50 -0700

    media class done

--- classes/Media.js
+++ classes/Media.js
@@ -1,4 +1,17 @@
 // create your Media class:
+class Media {
+    constructor(title, year, genre) {
+        this.title = title;
+        this.year = year;
+        this.genre = genre;
+    }
 
+    static totalMediaCount = 0;
+
+    summary() {
+        return `${this.title}, Year: ${this.year}, Genre: ${this.genre}`
+    }
+
+}
 // uncomment below to export it:
 module.exports = Media;
