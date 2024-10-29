2be407a5cad53bab83da0629a07dc1caeef02499
Paola Milanes <paola.milanes@paola.milanes-MacBook-Pro>
2024-10-29 13:01:52 -0700

    finished the second half

--- classes/Movie.js
+++ classes/Movie.js
@@ -1,5 +1,35 @@
 // import the Media class:
+const Media = require('./Media'); // Importing Media class
 
+class Movie extends Media {
+    constructor(title, year, genre, director, duration, rating) {
+      super(title, year, genre);
+      this.director = director; // Make sure this is assigned correctly
+      this.duration = duration; // Make sure this is assigned correctly
+      this.rating = rating; // Make sure this is assigned correctly
+    }
+  
+
+    // Method to validate the rating
+    validateRating(rating) {
+        if (rating < 1 || rating > 5) {
+            throw new Error("Rating must be between 1 and 5.");
+        }
+        return rating;
+    }
+
+    // Method to return a summary of the movie
+    summary() {
+        return `Title: ${this.title}, Director: ${this.director}, Year: ${this.year}, Genre: ${this.genre}, Rating: ${this.rating}`;
+      }
+
+    // Static method to find the longest movie
+    static longestMovie(movies) {
+        return movies.reduce((longest, current) => {
+          return (current.duration > longest.duration) ? current : longest;
+        });
+      }
+}
 // create your Movie class:
 
 // don't change below
--- classes/Music.js
+++ classes/Music.js
@@ -1,4 +1,34 @@
-// import the Media class:
+const Media = require('./Media'); // Importing Media class
+
+class Music extends Media {
+    constructor(title, year, genre, artist, length) {
+      super(title, year, genre);
+      this.artist = artist; // Make sure to assign this correctly
+      this.length = length; // Make sure to assign this correctly
+    }
+  
+
+    // Method to return a summary of the music
+    summary() {
+        return `Title: ${this.title}, Artist: ${this.artist}, Year: ${this.year}, Genre: ${this.genre}, Length: ${this.length} seconds`;
+      }
+
+    // Static method to find the shortest song in an album
+    static shortestAlbum(musicTracks) {
+        if (musicTracks.length === 0) return null; // Handle empty array
+        return musicTracks.reduce((shortest, current) => {
+            return current.length < shortest.length ? current : shortest;
+        });
+        
+    }
+    static calculateAverageRating(movies) {
+        const totalRating = movies.reduce((sum, movie) => sum + movie.rating, 0);
+        return totalRating / movies.length || 0; // Handle division by zero
+    }
+}
+
+
+ // Export the Movie class
 
 // create your Music class:
 
